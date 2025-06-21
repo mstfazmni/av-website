b74e68ed6e08a47934079c9671887c93777c4fb9
Mostafa Zamani <119905696+mstfazmni@users.noreply.github.com>
2025-06-21 15:37:18 -0600

    logo and text in Tab is updated

--- av-react-website/src/App.js
+++ av-react-website/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Home from './pages/Home';
 import './App.css';
+// import { Helmet } from 'react-helmet';
+
 
 function App() {
   return (
+    <>
+      {/* <Helmet>
+        <title>Calgary Renovation Services</title>
+        <meta name="description" content="Trusted Renovation Experts in Calgary" />
+      </Helmet> */}
     <Home />
+    </>
+  
   );
 }
 
